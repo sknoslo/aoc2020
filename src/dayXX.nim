94d037cc49b4d940be5bf36648529f4dfd328e94
Steffen Olson <sknoslo@gmail.com>
2020-11-26 11:03:56 -0500

    getting setup

---
+++ src/dayXX.nim
@@ -0,0 +1,24 @@
+## Template File ##
+import
+  strformat,
+  strutils
+
+proc part_one(input: string): string =
+  "INCOMPLETE"
+
+proc part_two(input: string): string =
+  "INCOMPLETE"
+
+when isMainModule:
+  echo "### DAY 01 ###"
+
+  let input = stdin.readAll.strip
+
+  echo "### INPUT ###"
+  echo input
+  echo "### END ###"
+
+  # do parsing here
+
+  echo(fmt"P1: {part_one(input)}")
+  echo(fmt"P2: {part_two(input)}")

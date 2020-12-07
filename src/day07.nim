fa26711fee8fdcb0a0e5a1a00dccb6c1291b45f5
Steffen Olson <solson@relias.com>
2020-12-07 12:08:05 -0500

    solve day07

--- src/day07.nim
+++ src/day07.nim
@@ -1,14 +1,18 @@
 import
   strformat,
   strutils,
+  sequtils,
   parseutils,
+  sets,
   tables,
   aoc2020pkg/bench
 
-type InnerBag = tuple[q: int, c: string] # maybe?
+type
+  InnerBag = tuple[q: int, c: string] # maybe?
+  Regulations = Table[string, seq[InnerBag]]
 
 # why write regex when you can write bad parsers?
-proc parseBags(s: string): Table[string, seq[InnerBag]] =
+proc parseRegs(s: string): Regulations =
   var pos = 0
   while pos < s.len:
     var
@@ -36,11 +40,42 @@ proc parseBags(s: string): Table[string, seq[InnerBag]] =
     pos += skipUntil(s, '.', pos) + 1
     pos += skipWhitespace(s, pos)
 
-proc partOne(input: string): string =
-  "INCOMPLETE"
+proc partOne(input: Regulations): string =
+  var inverted: Table[string, seq[string]]
+  for k in input.keys:
+    for inner in input[k]:
+      if not inverted.hasKey(inner.c):
+        inverted[inner.c] = @[]
+      inverted[inner.c].add(k)
 
-proc partTwo(input: string): string =
-  "INCOMPLETE"
+  var toVisit: seq[string]
+  for v in inverted["shiny gold"]:
+    toVisit.add(v)
+
+  var visited: HashSet[string]
+  while toVisit.len != 0:
+    let k = toVisit.pop
+    if visited.contains(k):
+      continue
+    visited.incl(k)
+    if not inverted.contains(k):
+      continue
+    for v in inverted[k]:
+      toVisit.add(v)
+
+  $visited.len
+
+proc partTwo(input: Regulations): string =
+  var
+    count = 0
+    toVisit = @[("shiny gold", 1)]
+
+  while toVisit.len != 0:
+    let (k, c) = toVisit.pop
+    for inner in input[k]:
+      count += c * inner.q
+      toVisit.add((inner.c, c * inner.q))
+  $count
 
 when isMainModule:
   echo "### DAY 01 ###"
@@ -51,9 +86,8 @@ when isMainModule:
   echo input
   echo "### END ###"
 
-  # do parsing here
-  echo parseBags(input)
+  let regs = parseRegs(input)
 
   benchmark:
-    echo(fmt"P1: {partOne(input)}")
-    echo(fmt"P2: {partTwo(input)}")
+    echo(fmt"P1: {partOne(regs)}")
+    echo(fmt"P2: {partTwo(regs)}")
